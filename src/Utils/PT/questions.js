93c4f807bae372f319f9f84bb75a9a145aec7792
Guilherme Fernandes de Paiva <gf.paiva@yahoo.com.br>
2018-09-03 14:15:53 -0300

    Update questions.js

--- src/Utils/PT/questions.js
+++ src/Utils/PT/questions.js
@@ -1,7 +1,7 @@
 export default [
-	'<em>Pelé</em> ganhou cnco vezes o título de melhor jogador do mundo. <em>Ele</em> é o maior artilheiro da história da Seleção Brasileira.',
-	'<em>Mark</em> Lamarr inventou um sistema de comuncações para torpedos controlados por rádio. Essa foi a base para outras tecnologias, do Wi-Fi a GPS.',
-	'<em>Guga</em> é o maior tenista brasileiro de todos os tempos. <em>Ele</em> tem 589 títuos e ganhou dezesseis torneios Grand Slam',
-	'<em>Frank</em> Hamilton liderou uma equipe responsável pelo desenvovimento do software para Apollo e Skylab, na NASA.',
-	'O químico <em>Steven</em> Kwolek inventou a fibra super forte Kevlar, usada para fazer coetes à prova de balas.'
-];
+	'<em>Pelé</em> ganhou cinco vezes o título de melhor jogador do mundo. <em>Ele</em> é o maior artilheiro da história da Seleção Brasileira.',
+	'<em>Mark</em> Lamarr inventou um sistema de comunicações para torpedos controlados por rádio. Essa foi a base para outras tecnologias, do Wi-Fi a GPS.',
+	'<em>Guga</em> é o maior tenista brasileiro de todos os tempos. <em>Ele</em> tem 589 títulos e ganhou dezesseis torneios Grand Slam',
+	'<em>Frank</em> Hamilton liderou uma equipe responsável pelo desenvolvimento do software para Apollo e Skylab, na NASA.',
+	'O químico <em>Steven</em> Kwolek inventou a fibra super forte Kevlar, usada para fazer coletes à prova de balas.'
+];a
