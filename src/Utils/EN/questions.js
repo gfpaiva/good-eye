8eb17256449c1a0962fcbc299b110bd18d39eaed
Guilherme Fernandes de Paiva <gf.paiva@yahoo.com.br>
2018-09-03 14:11:55 -0300

    Update questions.js

--- src/Utils/EN/questions.js
+++ src/Utils/EN/questions.js
@@ -1,7 +1,7 @@
 export default [
-	'<em>Pelé</em> has won five times the title of best player in the world. <em>He</em> is the greaest scorer in the history of the Brazilian National Team.',
-	'<em>Mark</em> Lamarr invented a secret communications system for radio-controlled torpedoes. This was the basis for other technologies, fom Wi-Fi to GPS.',
-	'<em>Guga</em> is the greatest Brazilian tennis player of all time. <em>He</em> has 589 tites and has won sixteen Grand Slam tournaments.',
-	'<em>Frank</em> Hamilton led a tean credited with developing the software for Apollo and Skylab, at NASA.',
-	'The chemist <em>Steven</em> Kwolek inventd the super-strong Kevlar fibre, used to make bulletproof vests.'
+	'<em>Pelé</em> has won five times the title of best player in the world. <em>He</em> is the greatest scorer in the history of the Brazilian National Team.',
+	'<em>Mark</em> Lamarr invented a secret communications system for radio-controlled torpedoes. This was the basis for other technologies, from Wi-Fi to GPS.',
+	'<em>Guga</em> is the greatest Brazilian tennis player of all time. <em>He</em> has 589 titles and has won sixteen Grand Slam tournaments.',
+	'<em>Frank</em> Hamilton led a team credited with developing the software for Apollo and Skylab, at NASA.',
+	'The chemist <em>Steven</em> Kwolek invented the super-strong Kevlar fibre, used to make bulletproof vests.'
 ];
