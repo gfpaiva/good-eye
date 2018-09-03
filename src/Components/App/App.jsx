7fabe500d40c545b0f1c3e60de341d83986f1a6a
Guilherme Paiva <gf.paiva@yahoo.com.br>
2018-09-03 02:30:37 -0300

    fix: text PT, layout footer welcome

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -47,6 +47,11 @@ class App extends Component {
 	componentWillMount() {
 		let vh = window.innerHeight * 0.01;
 		document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+		window.addEventListener('resize', () => {
+			let vh = window.innerHeight * 0.01;
+			document.documentElement.style.setProperty('--vh', `${vh}px`);
+		});
 	}
 
 	render() {
