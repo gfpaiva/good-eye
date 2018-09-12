cd4fcffd0be0b1ffe3345d4f04263121cd8e7a3e
gfpaiva <gf.paiva@yahoo.com.br>
2018-09-12 11:23:23 -0300

    fix: grammar question fix

--- src/Utils/EN/questions.js
+++ src/Utils/EN/questions.js
@@ -1,7 +1,7 @@
 export default [
 	'<em>Pelé</em> has won the title of the best player in the world five times. <em>He</em> is the greatest scorer in the history of the Brazilian National Team.',
 	'<em>Mark</em> Lamarr invented a secret communication system for radio-controlled torpedoes. The system was the basis for other technologies, from Wi-Fi to GPS.',
-	'<em>Guga</em> is the greatest Brazilian tennis player of all time. <em>He</em> has won sixteen Grand Slam tournaments, along with other 589 titles.',
+	'<em>Guga</em> is the greatest Brazilian tennis player of all time. <em>He</em> has won sixteen Grand Slam tournaments, along with 589 titles.',
 	'<em>Frank</em> Hamilton lead the NASA team that developed the software for Apollo and Skylab.',
 	'The chemist <em>Steven</em> Kwolek invented the super-strong Kevlar fiber, used to make bulletproof vests.'
 ];
